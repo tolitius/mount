4e5e4fba713f65bd6f221103e7f1fae818772f89
anatoly <tolitius@gmail.com>
2015-12-22 14:47:58 -0500

    [lein]: keeping a version upto date

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mount "0.1.7"
+(defproject mount "0.1.8-SNAPSHOT"
   :description "managing Clojure and ClojureScript app state since (reset)"
   :url "https://github.com/tolitius/mount"
   :license {:name "Eclipse Public License"
