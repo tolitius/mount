6dafae195d1cb7509d7e2d1c403996ddc4c25aae
anatoly <tolitius@gmail.com>
2019-01-28 10:34:26 -0500

    release 0.1.16

--- build.boot
+++ build.boot
@@ -1,4 +1,4 @@
-(def +version+ "0.1.16-SNAPSHOT")
+(def +version+ "0.1.17-SNAPSHOT")
 
 (set-env!
   :source-paths #{"src"}
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mount "0.1.16-SNAPSHOT"
+(defproject mount "0.1.17-SNAPSHOT"
   :description "managing Clojure and ClojureScript app state since (reset)"
   :url "https://github.com/tolitius/mount"
   :license {:name "Eclipse Public License"
