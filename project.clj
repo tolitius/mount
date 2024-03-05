55012bb6a1f482d25841cb3b932accae37b522ed
Eugen Stan <eugen@ieugen.ro>
2024-03-05 14:09:51 +0200

    Add resources to classpath
    Hopefully it will fix clj-kondo issue in https://github.com/tolitius/mount/pull/130

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src"]
+  :source-paths ["src" "resources"]
 
   :dependencies [] ;; for visual clarity
 
