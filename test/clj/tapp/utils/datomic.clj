4065f5df368893dcb4546fd80f573ee9f9226f85
Noah Bogart <noah.bogart@hey.com>
2024-07-11 11:41:52 -0400

    Update defstate clj-kondo hook to return a var

--- test/clj/tapp/utils/datomic.clj
+++ test/clj/tapp/utils/datomic.clj
@@ -6,8 +6,9 @@
 (defn entity [conn id]
   (d/entity (d/db conn) id))
 
-(defn touch [conn results]
+(defn touch
   "takes 'entity ids' results from a query
     e.g. '#{[272678883689461] [272678883689462] [272678883689459] [272678883689457]}'"
+  [conn results]
   (let [e (partial entity conn)]
     (map #(-> % first e d/touch) results)))
